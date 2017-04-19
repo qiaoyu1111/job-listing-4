6948bc1b19102f0f76641acf06a673c1c6fcac63
qiaoyu <qiaoyu1111358@gmail.com>
2017-04-19 17:50:32 +0800

    implement admin::tasks CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   resources :tasks
   devise_for :users
+
+  namespace :admin do
+    resources :tasks
+  end
+
+  
   root 'tasks#index'
   #root 'welcome#index'
 end
