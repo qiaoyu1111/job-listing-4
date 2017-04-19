f1edeb2d81c059889ae7c0c8c1da0e825f26651d
qiaoyu <qiaoyu1111358@gmail.com>
2017-04-19 15:53:59 +0800

    implement task CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
     @tasks = Task.all
   end
@@ -8,23 +10,35 @@ class TasksController < ApplicationController
   end
 
   def show
-
+    @task = Task.find(params[:id])
   end
 
   def edit
-
+    @task = Task.find(params[:id])
   end
 
   def create
-
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
   end
 
   def update
-
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
