7939b38fea8abf0346de7f7fbb53928ca6246ca5
qiaoyu <qiaoyu1111358@gmail.com>
2017-04-19 19:04:55 +0800

    add project_need

--- db/migrate/20170419104928_add_more_detail_to_task.rb
+++ db/migrate/20170419104928_add_more_detail_to_task.rb
@@ -1,5 +1,6 @@
 class AddMoreDetailToTask < ActiveRecord::Migration[5.0]
   def change
+    add_column :tasks, :project_need, :text
     add_column :tasks, :wage_upper_bound, :integer
     add_column :tasks, :wage_lower_bound, :integer
     add_column :tasks, :contact_email, :string
