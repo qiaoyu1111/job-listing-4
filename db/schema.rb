557e7fb36d9eaef427ee8751dff5f5a374a37183
qiaoyu <qiaoyu1111358@gmail.com>
2017-04-19 22:27:33 +0800

    add attachment to resume

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419141621) do
+ActiveRecord::Schema.define(version: 20170419142622) do
 
   create_table "resumes", force: :cascade do |t|
     t.integer  "task_id"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170419141621) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "attachment"
   end
 
   create_table "tasks", force: :cascade do |t|
