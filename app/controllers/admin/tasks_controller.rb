0d88645af029700aa15a6de680560948ee7122f2
qiaoyu <qiaoyu1111358@gmail.com>
2017-04-19 16:26:56 +0800

    implement admin::task CRUD

---
+++ app/controllers/admin/tasks_controller.rb
@@ -0,0 +1,49 @@
+class Admin::TasksController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "Update Success!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, alert: "Task deleted."
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :tasks
   devise_for :users
+
+  namespace :admin do
+    resources :tasks
+  end
+
   root 'tasks#index'
   #root 'welcome#index'
 end
